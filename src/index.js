99f5103d36c54ab824290fb2b1475aec37a2fde8
Marina <m.khmelyova@integer-soft.ru>
2020-10-22 10:40:14 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openingSymbols = [];
+    const closingSymbols = [];
+    const similarBrackets = [];
+    const bracketsPairs = [];
+    bracketsConfig.forEach((bracketsArray) => {
+        if (bracketsArray[1] === bracketsArray[0]) {
+            similarBrackets.push(bracketsArray[0]);
+        } else {
+            bracketsPairs[bracketsArray[1]] = bracketsArray[0];
+            openingSymbols.push(bracketsArray[0]);
+            closingSymbols.push(bracketsArray[1]);
+        }
+    });
+
+    const workArray = [];
+    let result = true;
+    str.split('').every((symbol) => {
+        if ((similarBrackets.includes(symbol) && !workArray.includes(symbol))
+            || (openingSymbols.includes(symbol))) {
+            workArray.push(symbol);
+        } else if ((similarBrackets.includes(symbol) && workArray.includes(symbol) && workArray[workArray.length - 1] === symbol)
+            || (closingSymbols.includes(symbol) && workArray[workArray.length - 1] === bracketsPairs[symbol])) {
+            workArray.pop();
+        } else if ((similarBrackets.includes(symbol) && workArray.includes(symbol) && !workArray[workArray.length - 1] === symbol)
+            || (closingSymbols.includes(symbol) && workArray[workArray.length - 1] !== bracketsPairs[symbol])) {
+            result = false;
+            return false;
+        }
+
+        return true;
+    });
+
+    return workArray.length > 0 ? false : result;
+};
